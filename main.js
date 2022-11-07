a2cf3f9aa2cdec348a60feda20a989a483e6aa6d
KaneAya <115447877+KaneAya@users.noreply.github.com>
2022-11-07 15:04:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,11 +10,9 @@ let img=[pic1.jpg,pic2.jpg,pic3.jpg,pic4.jpg,pic5.jpg]
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
-for(let i=0;i<5;i++){
-  console.log(img[i]);
+for(let i=1;i<=5;i++){
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/picS{i}.jpg`);
+  newImage.setAttribute('alt', xxx);
+  thumbBar.appendChild(newImage);
 }
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', "images/pic2.jpg");
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage2);
